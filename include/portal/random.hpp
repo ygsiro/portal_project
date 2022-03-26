d7ad1f64b6620e7eadd4c7bbd9b91ed20465cdd7
yagami siro <entoyukari@gmail.com>
2022-03-26 19:26:04 +0900

    int random

--- include/portal/random.hpp
+++ include/portal/random.hpp
@@ -152,6 +152,137 @@ private:
 };
 
 /**
+ * @brief uniform int distribution
+ *
+ * @tparam T floating-point type
+ * @tparam Engine engine
+ */
+template <std::integral T = int, std::uniform_random_bit_generator Engine = std::mt19937>
+class uniform_int_distribution {
+public:
+  using param_type  = std::uniform_int_distribution<T>::param_type;  //!< @param parameter
+  using result_type = std::uniform_int_distribution<T>::result_type; //!< @param result type
+
+  /**
+   * @brief Construct a new uniform int distribution object
+   *
+   */
+  uniform_int_distribution()
+      : uniform_int_distribution(static_cast<T>(0)) {
+  }
+
+  /**
+   * @brief Construct a new uniform int distribution object
+   *
+   * @param[in] a min value
+   * @param[in] b max value
+   *
+   * @pre a < b
+   */
+  explicit uniform_int_distribution(T a, T b = std::numeric_limits<T>::max())
+      : m_dist(a, b)
+      , m_engine(std::random_device{}()) {
+    assert(a < b);
+  }
+
+  /**
+   * @brief Construct a new uniform int distribution object
+   * 
+   * @param[in] parm parameter
+   */
+  explicit uniform_int_distribution(const param_type &parm)
+      : uniform_int_distribution(parm.a(), parm.b()) {
+  }
+
+  /**
+   * @brief reset
+   * 
+   */
+  void reset() {
+    m_dist.reset();
+  }
+
+  /**
+   * @brief min value
+   * 
+   * @return result_type 
+   */
+  result_type a() const {
+    return m_dist.a();
+  }
+
+  /**
+   * @brief max value
+   * 
+   * @return result_type 
+   */
+  result_type b() const {
+    return m_dist.b();
+  }
+
+  /**
+   * @brief min value
+   * 
+   * @return result_type 
+   */
+  result_type min() const {
+    return m_dist.min();
+  }
+
+  /**
+   * @brief max value
+   * 
+   * @return result_type 
+   */
+  result_type max() const {
+    return m_dist.max();
+  }
+
+  /**
+   * @brief parameter
+   * 
+   * @return param_type 
+   */
+  param_type param() const {
+    return m_dist.param;
+  }
+
+  /**
+   * @brief random
+   * 
+   * @param[in] engine engine
+   * @param[in] parm parameter
+   * @return result_type 
+   */
+  result_type operator()(const std::uniform_random_bit_generator auto &engine, const param_type &parm) {
+    return m_dist(engine, parm);
+  }
+
+  /**
+   * @brief random
+   * 
+   * @param[in] engine engine 
+   * @return result_type 
+   */
+  result_type operator()(const std::uniform_random_bit_generator auto &engine) {
+    return m_dist(engine);
+  }
+
+  /**
+   * @brief random
+   * 
+   * @return result_type 
+   */
+  result_type operator()() {
+    return m_dist(m_engine);
+  }
+
+private:
+  std::uniform_int_distribution<T> m_dist;
+  Engine m_engine = {};
+};
+
+/**
  * @brief random
  * 
  * @tparam T floating point type
